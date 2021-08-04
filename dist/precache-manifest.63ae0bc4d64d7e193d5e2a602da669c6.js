8e1d933527854043eba21dd7c1658e709b279a63
Rossy <86208497+rossyjs@users.noreply.github.com>
2021-08-04 14:05:30 +0300

    rossymain
    sj

---
+++ dist/precache-manifest.63ae0bc4d64d7e193d5e2a602da669c6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d922d80a8bac382bd3509cfc57cfad",
+    "url": "/bg.mp3"
+  },
+  {
+    "revision": "e8dbf1d50fe68a2a0e2f",
+    "url": "/css/about.844c3598.css"
+  },
+  {
+    "revision": "23c0725cbb0553cb6f5e",
+    "url": "/css/app.4fe40dbf.css"
+  },
+  {
+    "revision": "248b84fb2b817cfc29e0",
+    "url": "/css/chunk-vendors.acd9854f.css"
+  },
+  {
+    "revision": "9572d1927d46d80b8e8be24d3ec63b9c",
+    "url": "/fonts/font.9572d192.ttf"
+  },
+  {
+    "revision": "8e8c71c22b82f03a2cbc117d7d6fdc99",
+    "url": "/icon.png"
+  },
+  {
+    "revision": "ebf1eeeb074b9059b55acfc523c8ef68",
+    "url": "/img/bg.ebf1eeeb.jpg"
+  },
+  {
+    "revision": "54c1d573745e3a3f0f4f34a0d793421a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8dbf1d50fe68a2a0e2f",
+    "url": "/js/about.a3dea2a2.js"
+  },
+  {
+    "revision": "23c0725cbb0553cb6f5e",
+    "url": "/js/app.3037c451.js"
+  },
+  {
+    "revision": "626a574116007ff92d46",
+    "url": "/js/chunk-37a67e82.237e9092.js"
+  },
+  {
+    "revision": "248b84fb2b817cfc29e0",
+    "url": "/js/chunk-vendors.767dba33.js"
+  },
+  {
+    "revision": "37818bf608ccad42e44f72fc0a16bc25",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "7322a9f5afb3e3db88c543035848f518",
+    "url": "/netlify.toml"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
